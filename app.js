09bc3ccc74ca69296b9123bfee0f6a0694394dc4
Alessandra Valenti <alevalenti44@gmail.com>
2018-08-09 15:08:22 +0100

    Commented and cleaned obsolete code

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-var song;
-var fft;
-
-//var w is width of frequency bands
-var w;
-
-var boxSz;
-var gridSz;
-var zoom = false;
-var dropzone;
-var maxBoxSz;
-var minBoxSz;
-var zTranslate;
+let song;
+let fft;
+let boxSz;
+let gridSz;
+let dropzone;
+let pauseButton;
+let playButton;
+let maxBoxSz;
+let minBoxSz;
+let zTranslate;
 
 /*
 function preload() {
@@ -19,75 +16,86 @@ function preload() {
 */
 
 function setup() {
+  //File drop area + play and pause buttons
   dropzone = select('#dropzone');
-
+  pauseButton = select('#pauseButton');
+  playButton = select('#playButton');
   dropzone.dragOver(highlight);
-  function highlight(){
-    dropzone.style('background-color','#ccc');
+  function highlight() {
+    dropzone.style('background-color', '#ccc');
   }
 
   dropzone.dragLeave(unHighlight);
 
+  dropzone.drop(gotFile, unHighlight);
 
+  //Creates the canvas
+  const cnv = createCanvas(windowWidth, windowHeight, WEBGL);
 
-  dropzone.drop(gotFile,unHighlight);
+  cnv.style('display', 'block');
+  function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
 
-
-
-  var cnv = createCanvas(windowWidth, windowHeight, WEBGL);
-
-    cnv.style('display', 'block');
-    function windowResized() {
-        resizeCanvas(windowWidth, windowHeight);
-    }
-
-
-
+  //starts up audio analyzer
   amplitude = new p5.Amplitude();
   //0.9 smoothing and 64 frequency bands
   fft = new p5.FFT(0.9, 64);
-  //song.play();
-  //0.9 smoothing and 64 frequency bands
-  fft = new p5.FFT(0.9, 64);
 }
 
-function gotFile(file){
-    song = loadSound(file.data);
-    button = createButton('Play/Pause');
-    button.mousePressed(pauseMusic);
-    dropzone.style('visibility','hidden');
-
-
-}
-
-function unHighlight(){
-    dropzone.style('background-color','#fff');
-
-}
-
-function pauseMusic() {
-  if (song.isPlaying()) {
-    song.stop();
-  } else {
-    song.play();
+//This function executes when the mp3 has been dropped in the dropzone
+function gotFile(file) {
+  //Checks if file is mp3, if not gives error
+  console.log(file.type);
+  if(file.type != "audio"){
+    alert("File needs to be audio");
+  }else{
+      song = loadSound(file.data);
+      dropzone.style('visibility', 'hidden');
+      pauseButton.style('display', 'inline');
+      playButton.style('display', 'inline');
+      playButton.mousePressed(playMusic);
+      pauseButton.mousePressed(pauseMusic);
+      playButton.mouseOut(colorWhite);
+      pauseButton.mouseOut(colorWhite);
   }
 }
 
+function playMusic() {
+  song.play();
+  playButton.style('color', '#ccc');
+}
+
+function colorWhite() {
+  playButton.style('color', '#fff');
+  pauseButton.style('color', '#fff');
+}
+
+function pauseMusic() {
+  song.stop();
+  pauseButton.style('color', '#ccc');
+}
+
+function unHighlight() {
+  dropzone.style('background-color', '#fff');
+}
+
+//Draws the voxelized polygon
 function draw() {
   //Size of voxel sphere
   boxSz = 320;
   //Amount of voxels
-
-  var spectrum = fft.analyze();
-
-  var level = amplitude.getLevel();
+  let spectrum = fft.analyze();
+  //Gets level of amplitude
+  let level = amplitude.getLevel();
 
   //console.log(level);
   //console.log(spectrum);
 
+  //Changes amount of voxels depending on the frequency #
   ambientLight(spectrum[0]);
 
-  for (var i = 0; i < spectrum.length; i++) {
+  for (let i = 0; i < spectrum.length; i++) {
     if (spectrum[0] == 255) {
       gridSz = 255 / 8;
     } else if (spectrum[0] == 253) {
@@ -111,21 +119,23 @@ function draw() {
 
   zTranslate = -boxSz;
   background(0);
-
   translate(0, 0, zTranslate);
   push();
 
   //Speed of rotation
-  rotateY(frameCount * 0.01);
-  rotateZ(frameCount * 0.0125);
-  rotateX(frameCount * 0.02);
+  //Increase decimals to make shape spin faster
+  rotateY(frameCount  * 0.015);
+  rotateZ(spectrum[0] * 0.0012);
+  rotateX(frameCount  * 0.015);
 
-  var radius = boxSz - boxSz / 10;
+  let radius = boxSz - boxSz / 10;
 
-  for (var x = -boxSz + gridSz; x <= boxSz - gridSz; x += gridSz) {
-    for (var y = -boxSz + gridSz; y <= boxSz - gridSz; y += gridSz) {
-      for (var z = -boxSz + gridSz; z <= boxSz - gridSz; z += gridSz) {
-        var d = dist(0, 0, 0, x, y, z);
+
+  //Sets up voxels
+  for (let x = -boxSz + gridSz; x <= boxSz - gridSz; x += gridSz) {
+    for (let y = -boxSz + gridSz; y <= boxSz - gridSz; y += gridSz) {
+      for (let z = -boxSz + gridSz; z <= boxSz - gridSz; z += gridSz) {
+        let d = dist(0, 0, 0, x, y, z);
         if (d > radius - gridSz && d < radius) {
           push();
           translate(x, y, z);
